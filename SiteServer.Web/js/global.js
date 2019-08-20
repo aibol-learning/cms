ceb14d65623d29a5584c941817df18e26de664c0
yiran5467 <yiran_5467@163.com>
2019-08-20 17:25:13 +0800

    门户网站临时保存(今天完成了左右两侧悬浮窗的开发,待办任务接口未调通)

--- SiteServer.Web/js/global.js
+++ SiteServer.Web/js/global.js
@@ -3,7 +3,7 @@ var global = {
     newsSiteId: 36, //新闻中心网站id
     backstageAPIUrl: {//工作区接口访问url
         tours: 'http://backstage.aibol.com.cn/api/backstage/tours', //安全生产管理巡视接口url
-        Tasks: 'http://backstage.aibol.com.cn/api/backstage/tasks', //待办事项接口url
+        tasks: 'http://backstage.aibol.com.cn/api/backstage/tasks', //待办任务接口url
         loginInfo: 'http://backstage.aibol.com.cn/api/backstage/logininfo', //工作区当前登录信息接口Url
         screenData: ' http://screen.aibol.com.cn/home/data' //大屏幕数据接口Url
     },
@@ -11,4 +11,10 @@ var global = {
         // 正式服：http://10.138.20.219/tour/{id}
         // 内网测试服：http://10.138.20.219:82/tour/{id}
         // 外网测试服务：http://backstage.aibol.com.cn/tour/{id}
+    backstageTaskUrl: 'http://backstage.aibol.com.cn/',  //待办任务点击跳转的页面url
+        // 正式服：http://10.138.20.219/{redirectUrl}
+        // 内网测试服：http://10.138.20.219:82/{redirectUrl}
+        // 外网测试服务：http://backstage.aibol.com.cn/{redirectUrl}
+
+
 } 
