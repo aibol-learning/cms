6786fc5a7c9912804ad82278937ab1bae4c63df8
yiran5467 <yiran_5467@163.com>
2019-08-21 17:36:10 +0800

    报刊阅读详情页图片解析, 列表页加new标记, 首页加new标记, 待办任务浮窗位置调整, 左侧悬浮窗位置调整, 安全生产管理巡视去掉, 公告多显示5条

--- SiteServer.Web/js/global.js
+++ SiteServer.Web/js/global.js
@@ -1,7 +1,9 @@
 var global = {
     apikey: "38bfc679-ec19-488d-9877-c66016bbe2d5", //apikey
     newsSiteId: 36, //新闻中心网站id
+    loginInfo: null,  //当前登录信息
     backstageAPIUrl: {//工作区接口访问url
+        loginInfo: 'http://backstage.aibol.com.cn/api/backstage/logininfo', //当前登录信息接口url
         tours: 'http://backstage.aibol.com.cn/api/backstage/tours', //安全生产管理巡视接口url
         tasks: 'http://backstage.aibol.com.cn/api/backstage/tasks', //待办任务接口url
         loginInfo: 'http://backstage.aibol.com.cn/api/backstage/logininfo', //工作区当前登录信息接口Url
@@ -15,6 +17,4 @@ var global = {
         // 正式服：http://10.138.20.219/{redirectUrl}
         // 内网测试服：http://10.138.20.219:82/{redirectUrl}
         // 外网测试服务：http://backstage.aibol.com.cn/{redirectUrl}
-
-
 } 
