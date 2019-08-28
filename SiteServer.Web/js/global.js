1bccfe18e1316454c9e579014d159ed39a854879
Shawn Chen <cym@aibol.com>
2019-08-28 17:48:02 +0800

    添加前端站点的登录

--- SiteServer.Web/js/global.js
+++ SiteServer.Web/js/global.js
@@ -1,23 +1,42 @@
 var global = {
     apikey: "38bfc679-ec19-488d-9877-c66016bbe2d5", //apikey
     newsSiteId: 36, //新闻中心网站id
-    loginInfo: null,  //当前登录信息
-    sso: 'https://pass.aibol.com/backstage/identity/connect/authorize?client_id=backstage_cms_debug&redirect_uri=http://localhost:51687/&scope=openid%20profile&response_mode=form_post&response_type=id_token%20token&state=1234&nonce=abcd',
-    backstageAPIUrl: {//工作区接口访问url
-        loginInfo: 'http://backstage.aibol.com.cn/api/backstage/logininfo', //当前登录信息接口url
-        tours: 'http://backstage.aibol.com.cn/api/backstage/tours', //安全生产管理巡视接口url
-        tasks: 'http://backstage.aibol.com.cn/api/backstage/tasks', //待办任务接口url
-        screenData: ' http://screen.aibol.com.cn/home/data' //大屏幕数据接口Url
+    loginInfo: null, //当前登录信息
+    urls: {
+        backstage: 'http://backstage.aibol.com.cn/',
+        sso: 'https://pass.aibol.com/backstage/identity/'
     },
-    toursTaskUrl: 'http://backstage.aibol.com.cn/tour/',  //安全生产管理巡视任务点击跳转的页面url
-        // 正式服：http://10.138.20.219/tour/{id}
-        // 内网测试服：http://10.138.20.219:82/tour/{id}
-        // 外网测试服务：http://backstage.aibol.com.cn/tour/{id}
-    backstageTaskUrl: 'http://backstage.aibol.com.cn/',  //待办任务点击跳转的页面url
-        // 正式服：http://10.138.20.219/{redirectUrl}
-        // 内网测试服：http://10.138.20.219:82/{redirectUrl}
-        // 外网测试服务：http://backstage.aibol.com.cn/{redirectUrl}
-} 
+    // 单点登录配置
+    clients: {
+        // 新闻门户
+        portal: {
+            clientId: 'pw_portal_local',
+            redirectUri: escape('http://localhost:51687/api/aibol/idlogon')
+        },
+        // 新闻中心
+        news: {
+            clientId: 'pw_news',
+            redirectUri: escape('http://localhost:51687/newssite/')
+        },
+        getLoginUrl: function (clientId) {
+            var clientConfig = global.clients[clientId];
+
+            return global.urls.sso +
+                'connect/authorize?' +
+                'client_id=' + clientConfig.clientId +
+                '&redirect_uri=' + clientConfig.redirectUri +
+                '&scope=openid%20profile' +
+                '&response_mode=form_post' +
+                '&response_type=id_token%20token' +
+                '&state=' + new Date().getTime() +
+                '&nonce=' + clientId;
+        },
+        getLogoutUrl: function (clientId) {
+            var clientConfig = global.clients[clientId];
+            return "https://";
+        }
+    }
+};
 
 // 获取当前登录用户信息
 $.ajax({
