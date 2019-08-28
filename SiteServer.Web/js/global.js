66008713c601cad522383c09c16ab01e1e68dd2c
yiran5467 <yiran_5467@163.com>
2019-08-28 18:57:45 +0800

    大屏幕数据, 待办任务 接口联调

--- SiteServer.Web/js/global.js
+++ SiteServer.Web/js/global.js
@@ -4,8 +4,10 @@ var global = {
     loginInfo: null, //当前登录信息
     urls: {
         backstage: 'http://backstage.aibol.com.cn/',
-        sso: 'https://pass.aibol.com/backstage/identity/'
-    },
+        sso: 'https://pass.aibol.com/backstage/identity/',
+        backstageTasks: 'http://backstage.aibol.com.cn/tasks',  //待办任务查看更多点击跳转的页面url
+        backstageTask: 'http://backstage.aibol.com.cn/task/'         //待办任务点击跳转的页面url
+    }, 
     // 单点登录配置
     clients: {
         // 新闻门户
